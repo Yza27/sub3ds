e4c8fc4ac5d782188239703fb88668b95a5b927a
Luiz Carlos P. C. Mendes <61766359+lcpcmfafire@users.noreply.github.com>
2024-05-23 20:58:54 -0300

    Create conexao.php
    con v1

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+       DEFINE("host" , "............");
+	   DEFINE("usuario" , "host");
+	   DEFINE("senha" , " ");
+	   DEFINE("DB" , "db_dev");
+	   
+	   $_conexao = mysqli_connetc (rost,usuario,senha,DB) or
+	   ?>
